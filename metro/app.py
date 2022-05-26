3450feba5862340280a40abfe9a2bad0723f2911
north-star-saj <bsajevic22@gmail.com>
2022-05-25 20:55:57 -0500

    Add basic functionality for Route dropdown
    Resolves: Issue #2

--- metro/app.py
+++ metro/app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+)
+import requests
 
 app = Flask(__name__)
 
+data = [
+    {
+        "bus_route_id": "1",
+        "bus_route": "route1",
+    },
+    {"bus_route_id": "2", "bus_route": "route2"},
+]
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    data = get_routes()
+    return render_template("index.html", busroutes=data)
+
+
+def get_routes():
+    r = requests.get("https://svc.metrotransit.org/nextripv2/routes")
+    return r.json()
